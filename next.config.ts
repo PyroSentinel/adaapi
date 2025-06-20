9be18a0f42f64b583e81d743177726e218691700
Jordi Irawan <jordirwn@gmail.com>
2025-06-20 14:53:38 +0800

    initial

---
+++ next.config.ts
@@ -0,0 +1,14 @@
+import type { NextConfig } from 'next';
+
+const nextConfig: NextConfig = {
+  images: {
+    remotePatterns: [
+      { hostname: 'cdn.prod.website-files.com' },
+      { hostname: 'images.unsplash.com' },
+      { hostname: 'images.pexels.com' },
+      { hostname: 'picsum.photos' },
+      { hostname: 'placeimg.com' },
+    ],
+  },
+};
+export default nextConfig;
